b9fcb636e891ee72d3d937533b7360d959407579
Marina Rachlin <Marina.rachlin@gmail.com>
2024-01-26 08:46:04 +0200

    changes in django-settings

--- joblinkbackend/settings.py
+++ joblinkbackend/settings.py
@@ -178,7 +178,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
